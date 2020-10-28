fb7fa664304a1d4787cdf52c36018d5440ee932c
Gaurav Chaudhari <47390633+mrgauravchaudhari@users.noreply.github.com>
2020-10-28 09:48:17 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace itsppisapi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>().UseUrls("http://192.168.201.27:8085");
+            .UseStartup<Startup>().UseUrls("http://localhost:8085");
             // .UseStartup<Startup>().UseUrls("http://win-server:8085");
             // .UseStartup<Startup>().UseUrls("http://192.168.201.27:8085");
     }
